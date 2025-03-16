80c876d0579dc0e58b404ac5a8ef5061f4849d72
JihwanB <leyqorwlghks@gmail.com>
2025-03-16 20:10:34 +0900

    feat(map-service): add controllers, dtos
    - DirectionsController.java
    - GeocodingController.java
    - DirectionsResponseDto.java
    - GeocodingResponseDto.java

---
+++ map-service/src/main/java/org/onlogistics/mapservice/presentation/controller/GeocodingController.java
@@ -0,0 +1,25 @@
+package org.onlogistics.mapservice.presentation.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.onlogistics.mapservice.application.service.GeocodingService;
+import org.onlogistics.mapservice.global.presentation.dtos.CommonResponse;
+import org.onlogistics.mapservice.presentation.dtos.GeocodingResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GeocodingController {
+
+    private final GeocodingService geocodingService;
+
+    @GetMapping("/api/v1/maps/geocode")
+    public ResponseEntity<CommonResponse<?>> getCoordinates(
+        @RequestParam("query") String query) {
+        GeocodingResponseDto resDTO = geocodingService.getCoordinates(query);
+        return ResponseEntity.ok().body(CommonResponse.success(resDTO));
+    }
+
+}
