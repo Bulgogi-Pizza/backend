7d51cb2021b14baa0b4741454a9b781ea87bfe90
ss0ming <hee9920@naver.com>
2025-03-17 10:50:39 +0900

    feat: add Hub entity, vo, exception
    - Hub Entity 추가
    - Hub vo 추가
    - Hub Type enum 추가
    - Hub Exception 추가

---
+++ hub-service/src/main/java/on/logistics/hubservice/domain/entity/Hub.java
@@ -0,0 +1,56 @@
+package on.logistics.hubservice.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import on.logistics.hubservice.domain.entity.vo.Address;
+import on.logistics.hubservice.domain.entity.vo.Name;
+import on.logistics.hubservice.global.domain.BaseEntity;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_hub")
+public class Hub extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Embedded
+    private Name name;
+
+    @Enumerated(EnumType.STRING)
+    private HubType type;
+
+    @Embedded
+    private Address address;
+
+    @Column(nullable = false)
+    private BigDecimal latitude;  // 위도
+
+    @Column(nullable = false)
+    private BigDecimal longitude; // 경도
+
+    @Builder
+    private Hub(Name name, HubType type, Address address, BigDecimal latitude,
+        BigDecimal longitude) {
+        this.name = name;
+        this.type = type;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
