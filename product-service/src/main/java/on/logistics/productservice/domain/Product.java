e112f8dd9d1064ecbaee5b94016438f06450c211
hyper <revoluer9@gmail.com>
2025-03-17 19:29:52 +0900

    feat: add Company Entity

---
+++ product-service/src/main/java/on/logistics/productservice/domain/Product.java
@@ -0,0 +1,50 @@
+package on.logistics.productservice.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import on.logistics.productservice.domain.vo.Name;
+import on.logistics.productservice.domain.vo.Price;
+import on.logistics.productservice.domain.vo.Quantity;
+import on.logistics.productservice.global.domain.BaseEntity;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@SQLRestriction("is_deleted = false")
+@SQLDelete(sql = "UPDATE product SET is_deleted = true WHERE id = ?")
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "company_id")
+    private UUID companyId;
+
+    @Column(name = "managed_hub_id")
+    private UUID managedHubId;
+
+    @Embedded
+    private Name name;
+
+    @Embedded
+    private Quantity quantity;
+
+    @Embedded
+    private Price price;
+    private Long bundleSize;
+}
---
+++ product-service/src/main/java/on/logistics/productservice/domain/vo/Quantity.java
@@ -0,0 +1,36 @@
+package on.logistics.productservice.domain.vo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import on.logistics.productservice.exception.ProductException;
+import on.logistics.productservice.exception.ProductExceptionCode;
+
+@Getter
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Quantity {
+
+    @Column(name = "quantity", nullable = false)
+    private Long value;
+
+    public Quantity(final Long value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final Long value) {
+        if (value < 0) {
+            throw new ProductException(ProductExceptionCode.PRODUCT_QUANTITY_MIN);
+        }
+    }
+
+    public Quantity update(final Long value) {
+        return new Quantity(value);
+    }
+
+}
