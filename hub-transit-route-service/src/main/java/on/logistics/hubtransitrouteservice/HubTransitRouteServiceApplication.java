7152416b12a49c326bdb8abce2cb61af0f53749d
JihwanB <leyqorwlghks@gmail.com>
2025-03-17 23:12:54 +0900

    feat(hub-transit-route): init commit

---
+++ hub-transit-route-service/src/main/java/on/logistics/hubtransitrouteservice/HubTransitRouteServiceApplication.java
@@ -0,0 +1,13 @@
+package on.logistics.hubtransitrouteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubTransitRouteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubTransitRouteServiceApplication.class, args);
+	}
+
+}
---
+++ hub-transit-route-service/src/test/java/on/logistics/hubtransitrouteservice/HubTransitRouteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package on.logistics.hubtransitrouteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubTransitRouteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
