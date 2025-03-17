4194223db016872ac3c91085b218e979288314d5
JihwanB <leyqorwlghks@gmail.com>
2025-03-17 13:33:27 +0900

    chore(eureka-server): group명 변경
    - 그룹명을 on.logistics로 변경

--- eureka-server/src/main/java/on/logistics/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/on/logistics/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package org.onlogistics.eurekaserver;
+package on.logistics.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-server/src/test/java/on/logistics/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/on/logistics/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package org.onlogistics.eurekaserver;
+package on.logistics.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
