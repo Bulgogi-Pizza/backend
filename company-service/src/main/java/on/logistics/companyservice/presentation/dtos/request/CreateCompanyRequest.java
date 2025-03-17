b5293bda29f4a0e6955d395cf3c367f5292765bb
hyper <revoluer9@gmail.com>
2025-03-17 20:31:03 +0900

    fix: Company Create IO 수정

--- company-service/src/main/java/on/logistics/companyservice/presentation/dtos/request/CreateCompanyRequest.java
+++ company-service/src/main/java/on/logistics/companyservice/presentation/dtos/request/CreateCompanyRequest.java
@@ -6,15 +6,13 @@ import on.logistics.companyservice.application.dtos.request.CreateCompanyRequest
 import on.logistics.companyservice.domain.entity.enums.CompanyType;
 
 
-public record CreateCompanyRequest(@NotBlank @Size(max = 100) String companyName,
-                                   @NotBlank CompanyType companyType,
-                                   @NotBlank String companyAddress) {
+public record CreateCompanyRequest(
+    @NotBlank(message = "업체 이름은 반드시 입력되어야 합니다.") @Size(min = 1, max = 100, message = "업체 이름은 1자 이하, 100자 이상 초과할 수 없습니다.") String companyName,
+    @NotBlank(message = "업체 타입은 반드시 입력되어야 합니다.") CompanyType companyType,
+    @NotBlank(message = "업체 주소는 반드시 입력되어야 합니다.") String companyAddress) {
 
     public static CreateCompanyRequestDto from(CreateCompanyRequest dto) {
-        return new CreateCompanyRequestDto(
-            dto.companyName(),
-            dto.companyType(),
-            dto.companyAddress()
-        );
+        return new CreateCompanyRequestDto(dto.companyName(), dto.companyType(),
+            dto.companyAddress());
     }
 }
