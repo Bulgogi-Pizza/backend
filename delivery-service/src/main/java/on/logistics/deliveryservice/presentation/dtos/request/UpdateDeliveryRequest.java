b29df918bf5f71ed6b46a20289072d46160513ba
hyper <revoluer9@gmail.com>
2025-03-18 21:31:49 +0900

    style: 변수명 변경

--- delivery-service/src/main/java/on/logistics/deliveryservice/presentation/dtos/request/UpdateDeliveryRequest.java
+++ delivery-service/src/main/java/on/logistics/deliveryservice/presentation/dtos/request/UpdateDeliveryRequest.java
@@ -3,9 +3,10 @@ package on.logistics.deliveryservice.presentation.dtos.request;
 import java.util.UUID;
 import on.logistics.deliveryservice.application.dtos.request.UpdateDeliveryRequestDto;
 
-public record UpdateDeliveryRequest(UUID orderId, String description) {
+public record UpdateDeliveryRequest(UUID deliveryId, String description) {
 
     public static UpdateDeliveryRequestDto from(UUID deliveryId, UpdateDeliveryRequest request) {
-        return new UpdateDeliveryRequestDto(deliveryId, request.orderId(), request.description());
+        return new UpdateDeliveryRequestDto(deliveryId, request.deliveryId(),
+            request.description());
     }
 }
