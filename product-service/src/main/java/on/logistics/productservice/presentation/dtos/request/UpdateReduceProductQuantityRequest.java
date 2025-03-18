238bf00a96ff94d047ea4465e1f0e10f3f3d9929
hyper <revoluer9@gmail.com>
2025-03-18 13:04:15 +0900

    chore: UpdateReduceProductQuantityRequest 에러 메시지 변경

--- product-service/src/main/java/on/logistics/productservice/presentation/dtos/request/UpdateReduceProductQuantityRequest.java
+++ product-service/src/main/java/on/logistics/productservice/presentation/dtos/request/UpdateReduceProductQuantityRequest.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import on.logistics.productservice.application.dto.UpdateReduceProductQuantityRequestDto;
 
 public record UpdateReduceProductQuantityRequest(
-    @Min(value = 1, message = "재고 수량은 한 개 미만일 수 없습니다.") Long productQuantity) {
+    @Min(value = 1, message = "재고 수량 감소는 한 개 미만일 수 없습니다.") Long productQuantity) {
 
     public static UpdateReduceProductQuantityRequestDto from(UUID productId,
         UpdateReduceProductQuantityRequest updateReduceProductQuantityRequest) {
