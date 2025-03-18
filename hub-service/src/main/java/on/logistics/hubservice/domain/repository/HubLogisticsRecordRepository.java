5f422b66fde1cd4065abfdd25aacbc4d7068082a
ss0ming <hee9920@naver.com>
2025-03-18 15:24:50 +0900

    feat: add Hub Logistics retrieval

--- hub-service/src/main/java/on/logistics/hubservice/domain/repository/HubLogisticsRecordRepository.java
+++ hub-service/src/main/java/on/logistics/hubservice/domain/repository/HubLogisticsRecordRepository.java
@@ -1,6 +1,7 @@
 package on.logistics.hubservice.domain.repository;
 
 import java.util.List;
+import java.util.UUID;
 import on.logistics.hubservice.domain.entity.HubLogisticsRecord;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface HubLogisticsRecordRepository {
 
     void saveAll(List<HubLogisticsRecord> records);
+
+    List<HubLogisticsRecord> findAllByDeliveryIdIn(List<UUID> retrievalLogisticsIds);
 }
