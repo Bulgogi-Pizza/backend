26999412d5d8714b36c1c23b5fef98d9f78f3beb
hyper <revoluer9@gmail.com>
2025-03-18 11:41:08 +0900

    fix: Common Pageable 수정

--- company-service/src/main/java/on/logistics/companyservice/global/configuration/PageableConfig.java
+++ company-service/src/main/java/on/logistics/companyservice/global/configuration/PageableConfig.java
@@ -2,14 +2,8 @@ package on.logistics.companyservice.global.configuration;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import on.logistics.companyservice.global.enums.PageNumber;
-import on.logistics.companyservice.global.enums.PageSize;
-import on.logistics.companyservice.global.enums.PageSortBy;
 import on.logistics.companyservice.global.resolver.PageableVerificationArgumentResolver;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,20 +16,5 @@ public class PageableConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(pageableVerificationArgumentResolver);
-        resolvers.add(fallbackPageableResolver());
     }
-
-    public PageableHandlerMethodArgumentResolver fallbackPageableResolver() {
-        final PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
-        resolver.setFallbackPageable(PageRequest.of(
-            PageNumber.MINIMUM_PAGE_NUMBER.getNumber(),
-            PageSize.DEFAULT.getSize(),
-            Direction.DESC,
-            PageSortBy.CREATED_AT.getSortBy(),
-            PageSortBy.UPDATED_AT.getSortBy(),
-            PageSortBy.ID.getSortBy()
-        ));
-        return resolver;
-    }
-
 }
