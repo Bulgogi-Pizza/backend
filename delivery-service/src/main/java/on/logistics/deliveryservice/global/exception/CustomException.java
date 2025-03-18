52dfe631d527c0793a110f0488a4fb1ae5f72bbe
hyper <revoluer9@gmail.com>
2025-03-18 15:08:45 +0900

    feat: Initialize delivery service project

---
+++ delivery-service/src/main/java/on/logistics/deliveryservice/global/exception/CustomException.java
@@ -0,0 +1,15 @@
+package on.logistics.deliveryservice.global.exception;
+
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ExceptionCode exception;
+
+    public CustomException(ExceptionCode e) {
+        super(e.getMessage());
+        this.exception = e;
+    }
+}
