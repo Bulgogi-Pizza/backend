38f37109b023595d1f407a91aa7798b2cf41f31c
hyper <revoluer9@gmail.com>
2025-03-19 19:57:37 +0900

    fix: update ActualDeliveryRecord 버그 수정

--- delivery-service/src/main/java/on/logistics/deliveryservice/presentation/dtos/request/UpdateDeliveryRecordRequest.java
+++ delivery-service/src/main/java/on/logistics/deliveryservice/presentation/dtos/request/UpdateDeliveryRecordRequest.java
@@ -1,9 +1,9 @@
 package on.logistics.deliveryservice.presentation.dtos.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record UpdateDeliveryRecordRequest(
-    @NotBlank(message = "actualDistance 는 필수 입력 값입니다.") Long actualDistance,
-    @NotBlank(message = "actualDistance 는 필수 입력 값입니다.") Long actualDuration) {
+    @NotNull(message = "actualDistance 는 필수 입력 값입니다.") Long actualDistance,
+    @NotNull(message = "actualDistance 는 필수 입력 값입니다.") Long actualDuration) {
 
 }
