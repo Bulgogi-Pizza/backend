772bdd6145c76a4f14c41b40790dd595ecb5e0e4
hyper <revoluer9@gmail.com>
2025-03-19 23:26:28 +0900

    feat: Product 생성 시 Company 검증 및 매니저 허브 지정

--- product-service/src/main/java/on/logistics/productservice/domain/dto/CreateProductDto.java
+++ product-service/src/main/java/on/logistics/productservice/domain/dto/CreateProductDto.java
@@ -2,13 +2,15 @@ package on.logistics.productservice.domain.dto;
 
 import java.util.UUID;
 import on.logistics.productservice.application.dto.CreateProductRequestDto;
+import on.logistics.productservice.infrastructure.clients.company.feign.dtos.GetCompanyInfo;
 
 public record CreateProductDto(String productName, UUID companyId, UUID managedHubId,
                                Long productQuantity, Long productPrice, Long bundleSize) {
 
-    public static CreateProductDto from(CreateProductRequestDto requestDto) {
+    public static CreateProductDto from(CreateProductRequestDto requestDto,
+        GetCompanyInfo getCompanyInfo) {
         return new CreateProductDto(requestDto.productName(), requestDto.companyId(),
-            requestDto.managedHubId(),
-            requestDto.productQuantity(), requestDto.productPrice(), requestDto.bundleSize());
+            getCompanyInfo.managedHubId(), requestDto.productQuantity(), requestDto.productPrice(),
+            requestDto.bundleSize());
     }
 }
