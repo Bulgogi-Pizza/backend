438a58a4ea576ee4c3ed266f1e18a9bb4d17b29b
hyper <revoluer9@gmail.com>
2025-03-20 02:27:47 +0900

    feat: CompanyService Add UpdateCompanyUser Validation

--- company-service/src/main/java/on/logistics/companyservice/presentation/dtos/request/UpdateCompanyUserRequest.java
+++ company-service/src/main/java/on/logistics/companyservice/presentation/dtos/request/UpdateCompanyUserRequest.java
@@ -1,7 +1,8 @@
 package on.logistics.companyservice.presentation.dtos.request;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
-public record UpdateCompanyUserRequest(UUID userId) {
+public record UpdateCompanyUserRequest(@NotNull(message = "userId는 필수 입력 값입니다.") UUID userId) {
 
 }
