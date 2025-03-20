cc6e1ac4afb827fb30c794e45a4f865906df56d8
hyper <revoluer9@gmail.com>
2025-03-21 00:40:19 +0900

    feat: CompanyService JpaAuditing AuditorAware 설정 추가

---
+++ company-service/src/main/java/on/logistics/companyservice/global/domain/Passport.java
@@ -0,0 +1,15 @@
+package on.logistics.companyservice.global.domain;
+
+import java.util.UUID;
+import lombok.Getter;
+
+@Getter
+public class Passport {
+
+    private UUID userId;
+    private String username;
+    private String role;
+    private String nickname;
+    private String slackEmail;
+
+}
