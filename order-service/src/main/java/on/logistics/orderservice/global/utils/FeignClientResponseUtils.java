fbe09576d81314603bd30eccd88a57b9e7ef0cd6
jeonjuho <wjswngh23@daum.net>
2025-03-20 19:03:51 +0900

    feat: FeignClient 사용 시 List 응답 파싱 추가
    - List 객체로 응답되는 데이터 파싱 추가

--- order-service/src/main/java/on/logistics/orderservice/global/utils/FeignClientResponseUtils.java
+++ order-service/src/main/java/on/logistics/orderservice/global/utils/FeignClientResponseUtils.java
@@ -1,8 +1,10 @@
 package on.logistics.orderservice.global.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Response;
 import java.io.IOException;
+import java.util.List;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import on.logistics.orderservice.infrastructure.clients.exception.ExternalApiException.ExternalApiBadRequestException;
@@ -21,6 +23,11 @@ public class FeignClientResponseUtils {
   public static <T> T getBody(Response response, Class<T> responseType) {
     validateResponseStatus(response);
     return parseResponseBody(response, responseType);
+  }
+
+  public static List getListBody(Response response, Class<?> responseType) {
+    validateResponseStatus(response);
+    return parseResponseListBody(response, responseType);
   }
 
   public static void validateResponseStatus(Response response) {
@@ -42,20 +49,66 @@ public class FeignClientResponseUtils {
   public static <T> T parseResponseBody(Response response, Class<T> responseType) {
     log.info("응답 바디 파싱");
     try {
-      CommonResponse commonResponse = objectMapper.readValue(
-          response.body().asInputStream(),
-          objectMapper.getTypeFactory().constructType(CommonResponse.class));
+      CommonResponse commonResponse = parseCommonResponse(response);
       log.info("응답: {}", commonResponse);
       if (commonResponse == null) {
         return null;
       }
-      return objectMapper.readValue(
-          objectMapper.writeValueAsString(commonResponse.data()),
-          responseType
-      );
+      return parseCommonResponseToT(responseType, commonResponse);
     } catch (IOException e) {
       log.error("잘못된 응답 형식입니다.", e);
       throw new WrongResponseTypeApiException();
     }
+  }
+
+  private static CommonResponse parseCommonResponse(Response response) throws IOException {
+    return objectMapper.readValue(
+        response.body().asInputStream(),
+        objectMapper.getTypeFactory().constructType(CommonResponse.class));
+  }
+
+  private static <T> T parseCommonResponseToT(Class<T> responseType, CommonResponse commonResponse)
+      throws JsonProcessingException {
+    return objectMapper.readValue(
+        objectMapper.writeValueAsString(commonResponse.data()),
+        responseType
+    );
+  }
+
+  public static <T> List parseResponseListBody(Response response, Class<T> responseType) {
+    log.info("List 응답 바디 파싱");
+    try {
+      CommonResponse commonResponse = parseCommonResponse(response);
+      log.info("응답: {}", commonResponse);
+      if (commonResponse == null) {
+        return null;
+      }
+      List list = parseCommonResponseToList(commonResponse);
+      return list.stream().map(o -> {
+        try {
+          return parseObjectToT(responseType, o);
+        } catch (IOException e) {
+          log.error("잘못된 응답 형식입니다.", e);
+          throw new WrongResponseTypeApiException();
+        }
+      }).toList();
+    } catch (IOException e) {
+      log.error("잘못된 응답 형식입니다.", e);
+      throw new WrongResponseTypeApiException();
+    }
+  }
+
+  private static List parseCommonResponseToList(CommonResponse commonResponse) throws JsonProcessingException {
+    return objectMapper.readValue(
+        objectMapper.writeValueAsString(commonResponse.data()),
+        List.class
+    );
+  }
+
+  private static <T> T parseObjectToT(Class<T> responseType, Object o) throws JsonProcessingException {
+    return objectMapper.readValue(
+        objectMapper.writeValueAsString(o),
+        responseType
+    );
   }
 }
