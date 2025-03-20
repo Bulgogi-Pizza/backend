9fa43d4eb0f165656093a713bba48e3a34c0b1aa
jeonjuho <wjswngh23@daum.net>
2025-03-20 22:40:47 +0900

    feat: init ai service project

---
+++ ai-service/src/test/java/on/logistics/aiservice/AiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package on.logistics.aiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
