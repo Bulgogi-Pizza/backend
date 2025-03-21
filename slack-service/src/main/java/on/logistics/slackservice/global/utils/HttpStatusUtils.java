9fbe5ec7ba529c41e03aa28dcab074f39e6fb04e
JihwanB <leyqorwlghks@gmail.com>
2025-03-21 15:00:22 +0900

    feat(slack-service): service init commit

---
+++ slack-service/src/main/java/on/logistics/slackservice/global/utils/HttpStatusUtils.java
@@ -0,0 +1,29 @@
+package on.logistics.slackservice.global.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class HttpStatusUtils {
+
+    public static boolean is2xxSuccessful(int statusCode) {
+        return statusCode >= 200 && statusCode < 300;
+    }
+
+    public static boolean is4xxClientError(int statusCode) {
+        return statusCode >= 400 && statusCode < 500;
+    }
+
+    public static boolean isResponseBadRequest(int status) {
+        return status == 400;
+    }
+
+    public static boolean isResponseNotFound(int status) {
+        return status == 404;
+    }
+
+    public static boolean is5xxServerError(int statusCode) {
+        return statusCode >= 500 && statusCode < 600;
+    }
+
+}
