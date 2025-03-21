4a1cd83460e78841ad4efb40a42bba42bf16496a
ss0ming <hee9920@naver.com>
2025-03-21 12:39:25 +0900

    feat: add feign client request to delivery manager service

--- hub-service/src/main/java/on/logistics/hubservice/application/service/HubLogisticsRecordService.java
+++ hub-service/src/main/java/on/logistics/hubservice/application/service/HubLogisticsRecordService.java
@@ -3,6 +3,8 @@ package on.logistics.hubservice.application.service;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import on.logistics.hubservice.application.dtos.request.AssignDeliveryManagerRequestDto;
 import on.logistics.hubservice.application.dtos.request.RetrievalLogisticsRequestDto;
 import on.logistics.hubservice.application.dtos.request.StorageLogisticsRequestDto;
 import on.logistics.hubservice.domain.entity.HubLogisticsRecord;
@@ -10,15 +12,18 @@ import on.logistics.hubservice.domain.repository.HubLogisticsRecordRepository;
 import on.logistics.hubservice.domain.repository.HubRepository;
 import on.logistics.hubservice.exception.HubException;
 import on.logistics.hubservice.exception.HubExceptionCode;
+import on.logistics.hubservice.infrastructure.clients.deliverymanager.DeliveryManagerServiceClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class HubLogisticsRecordService {
 
     private final HubLogisticsRecordRepository hubLogisticsRecordRepository;
     private final HubRepository hubRepository;
+    private final DeliveryManagerServiceClient deliveryManagerServiceClient;
 
     @Transactional
     public void storage(StorageLogisticsRequestDto requestDto) {
@@ -29,6 +34,8 @@ public class HubLogisticsRecordService {
             .map(id -> HubLogisticsRecord.storage(requestDto.hubId(), id))
             .toList();
         hubLogisticsRecordRepository.saveAll(records);
+
+        assignDeliveryManager(requestDto);
     }
 
     public void retrieval(RetrievalLogisticsRequestDto requestDto) {
@@ -43,5 +50,22 @@ public class HubLogisticsRecordService {
         if (!hubRepository.existsById(hubId)) {
             throw new HubException(HubExceptionCode.HUB_NOT_FOUND);
         }
+    }
+
+    private void assignDeliveryManager(
+        StorageLogisticsRequestDto logistics) {
+        logistics.storageLogisticsIds().stream()
+            .forEach(deliverId -> {
+                // TODO: 배송 간 이동정보로 다음 목적지와 배송타입을 받아와야함
+                final var requestDto = AssignDeliveryManagerRequestDto.of(
+                    deliverId,
+                    logistics.hubId(),
+                    "COMPANY_DELIVERY"); // TODO: 다음 목적지에 대한 값 받을 때 응답으로 온 데이터 넣기
+                final var assignedDeliveryManager = deliveryManagerServiceClient.assignDeliveryManager(
+                    requestDto);
+                log.info(
+                    "assingedDeliveryManager ID: " + assignedDeliveryManager.deliveryManagerId());
+                // TODO: 배송 간 이동정보로 deliveryMangaerId (배정된 배송담당자)를 보내줘야함
+            });
     }
 }
---
+++ hub-service/src/main/java/on/logistics/hubservice/infrastructure/clients/deliverymanager/DeliveryManagerServiceClientImpl.java
@@ -0,0 +1,25 @@
+package on.logistics.hubservice.infrastructure.clients.deliverymanager;
+
+import feign.Response;
+import lombok.RequiredArgsConstructor;
+import on.logistics.hubservice.application.dtos.request.AssignDeliveryManagerRequestDto;
+import on.logistics.hubservice.global.utills.FeignClientResponseUtils;
+import on.logistics.hubservice.infrastructure.clients.deliverymanager.feign.DeliveryManagerServiceFeignClient;
+import on.logistics.hubservice.infrastructure.clients.deliverymanager.feign.dtos.request.AssignDeliveryManagerRequest;
+import on.logistics.hubservice.infrastructure.clients.deliverymanager.feign.dtos.response.AssignDeliveryManagerResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryManagerServiceClientImpl implements DeliveryManagerServiceClient {
+
+    private final DeliveryManagerServiceFeignClient deliveryManagerServiceFeignClient;
+
+    @Override
+    public AssignDeliveryManagerResponse assignDeliveryManager(
+        AssignDeliveryManagerRequestDto requestDto) {
+        final var request = AssignDeliveryManagerRequest.of(requestDto);
+        Response response = deliveryManagerServiceFeignClient.assignDeliveryManager(request);
+        return FeignClientResponseUtils.getBody(response, AssignDeliveryManagerResponse.class);
+    }
+}
