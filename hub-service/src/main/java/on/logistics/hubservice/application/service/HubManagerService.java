ee1ce3fe31a646729583d6c0cd2e90b8b763c011
ss0ming <hee9920@naver.com>
2025-03-22 17:11:11 +0900

    feat: modify passport utils

--- hub-service/src/main/java/on/logistics/hubservice/application/service/HubManagerService.java
+++ hub-service/src/main/java/on/logistics/hubservice/application/service/HubManagerService.java
@@ -5,6 +5,7 @@ import on.logistics.hubservice.application.dtos.request.CreateHubManagerRequestD
 import on.logistics.hubservice.application.dtos.request.ValidHubManagerRequestDto;
 import on.logistics.hubservice.domain.entity.HubManager;
 import on.logistics.hubservice.domain.repository.HubManagerRepository;
+import on.logistics.hubservice.global.util.PassportUtil;
 import on.logistics.hubservice.presentation.dtos.response.CreateHubManagerResponse;
 import on.logistics.hubservice.presentation.dtos.response.ValidHubManagerResponse;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class HubManagerService {
 
     private final HubManagerRepository hubManagerRepository;
+    private final PassportUtil passportUtil;
 
     @Transactional
-    public CreateHubManagerResponse createHubManager(CreateHubManagerRequestDto requestDto) {
+    public CreateHubManagerResponse createHubManager(CreateHubManagerRequestDto requestDto
+    ) {
         HubManager hubManager = HubManager.create(requestDto);
         HubManager savedHubManager = hubManagerRepository.save(hubManager);
         return CreateHubManagerResponse.of(savedHubManager.getId());
