e193d3e44c1af202ef7654a4892f17cde14cc0eb
JihwanB <leyqorwlghks@gmail.com>
2025-03-23 16:48:42 +0900

    feat(hub-transit-service): FeignClientConfig에 RequestInterceptor를 추가

--- hub-transit-service/src/main/java/on/logistics/hubtransitservice/global/configuration/FeignClientConfig.java
+++ hub-transit-service/src/main/java/on/logistics/hubtransitservice/global/configuration/FeignClientConfig.java
@@ -1,10 +1,30 @@
 package on.logistics.hubtransitservice.global.configuration;
 
+import feign.RequestInterceptor;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 @EnableFeignClients(basePackages = "on.logistics.hubtransitservice.infrastructure.clients")
 public class FeignClientConfig {
+
+    @Bean
+    public RequestInterceptor feignRequestInterceptor() {
+        return template -> {
+            ServletRequestAttributes attributes =
+                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes != null) {
+                HttpServletRequest request = attributes.getRequest();
+                String passportId = request.getHeader("X-Passport-Id");
+                if (passportId != null) {
+                    template.header("X-Passport-Id", passportId);
+                }
+            }
+        };
+    }
 
 }
