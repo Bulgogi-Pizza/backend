65252500c049440826eed1b1fc7c0a04ef425ff2
hyper <revoluer9@gmail.com>
2025-03-23 20:00:39 +0900

    feat: ProductService FeignClientConfig 추가

--- product-service/src/main/java/on/logistics/productservice/infrastructure/clients/hub/feign/HubServiceFeignClient.java
+++ product-service/src/main/java/on/logistics/productservice/infrastructure/clients/hub/feign/HubServiceFeignClient.java
@@ -2,6 +2,7 @@ package on.logistics.productservice.infrastructure.clients.hub.feign;
 
 import feign.Response;
 import java.util.UUID;
+import on.logistics.productservice.global.configuration.FeignClientConfig;
 import on.logistics.productservice.infrastructure.clients.hub.feign.dtos.HubManagerBooleanRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "hub-service")
+@FeignClient(name = "hub-service", configuration = FeignClientConfig.class)
 public interface HubServiceFeignClient {
 
     @GetMapping("/api/v1/hubs/{hubId}")
