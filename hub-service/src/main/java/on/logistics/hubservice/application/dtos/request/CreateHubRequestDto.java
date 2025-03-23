7b4852e061127f83f24114209f14cba72036e04d
ss0ming <hee9920@naver.com>
2025-03-23 21:23:52 +0900

    feat: add hub service validate auth

--- hub-service/src/main/java/on/logistics/hubservice/application/dtos/request/CreateHubRequestDto.java
+++ hub-service/src/main/java/on/logistics/hubservice/application/dtos/request/CreateHubRequestDto.java
@@ -1,5 +1,6 @@
 package on.logistics.hubservice.application.dtos.request;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.Builder;
 import on.logistics.hubservice.domain.entity.HubType;
 import on.logistics.hubservice.presentation.dtos.request.CreateHubRequest;
@@ -8,14 +9,17 @@ import on.logistics.hubservice.presentation.dtos.request.CreateHubRequest;
 public record CreateHubRequestDto(
     String hubName,
     HubType hubType,
-    String hubAddress
+    String hubAddress,
+    HttpServletRequest passportRequest
 ) {
 
-    public static CreateHubRequestDto of(CreateHubRequest request) {
+    public static CreateHubRequestDto of(CreateHubRequest request,
+        HttpServletRequest passportRequest) {
         return CreateHubRequestDto.builder()
             .hubName(request.hubName())
             .hubType(HubType.valueOf(request.hubType()))
             .hubAddress(request.hubAddress())
+            .passportRequest(passportRequest)
             .build();
     }
 }
--- hub-service/src/main/java/on/logistics/hubservice/application/dtos/request/UpdateHubRequestDto.java
+++ hub-service/src/main/java/on/logistics/hubservice/application/dtos/request/UpdateHubRequestDto.java
@@ -1,5 +1,6 @@
 package on.logistics.hubservice.application.dtos.request;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.UUID;
 import lombok.Builder;
 import on.logistics.hubservice.domain.entity.HubType;
@@ -10,15 +11,18 @@ public record UpdateHubRequestDto(
     UUID id,
     String hubName,
     HubType hubType,
-    String hubAddress
+    String hubAddress,
+    HttpServletRequest passportRequest
 ) {
 
-    public static UpdateHubRequestDto of(UUID id, UpdateHubRequest request) {
+    public static UpdateHubRequestDto of(UUID id, UpdateHubRequest request,
+        HttpServletRequest passportRequest) {
         return UpdateHubRequestDto.builder()
             .id(id)
             .hubName(request.hubName())
             .hubType(HubType.valueOf(request.hubType()))
             .hubAddress(request.hubAddress())
+            .passportRequest(passportRequest)
             .build();
     }
 
