3512090191a083a71450dc1a86e31c8a863c1ffb
jeonjuho <wjswngh23@daum.net>
2025-03-24 12:39:30 +0900

    fix: 허브 관리자 검증 요청 dto 수정

--- order-service/src/main/java/on/logistics/orderservice/infrastructure/clients/hub/feign/dtos/ValidateHubManagerRequest.java
+++ order-service/src/main/java/on/logistics/orderservice/infrastructure/clients/hub/feign/dtos/ValidateHubManagerRequest.java
@@ -3,11 +3,11 @@ package on.logistics.orderservice.infrastructure.clients.hub.feign.dtos;
 import java.util.UUID;
 
 public record ValidateHubManagerRequest(
-    UUID companyId,
+    UUID userId,
     UUID hubId
 ) {
 
-    public static ValidateHubManagerRequest of(UUID companyId, UUID hubId) {
-        return new ValidateHubManagerRequest(companyId, hubId);
+    public static ValidateHubManagerRequest of(UUID userId, UUID hubId) {
+        return new ValidateHubManagerRequest(userId, hubId);
     }
 }
