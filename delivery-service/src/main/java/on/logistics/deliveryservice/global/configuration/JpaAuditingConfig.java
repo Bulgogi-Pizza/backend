304b5b46c3e1bc332d900469aa7fd4be89dfa1c7
Bulgogi-Pizza <quitendexit@gmail.com>
2025-03-25 10:23:21 +0900

    fix: 인증/사용자 테스트 중 오류사항 수정 exception code, security config

--- delivery-service/src/main/java/on/logistics/deliveryservice/global/configuration/JpaAuditingConfig.java
+++ delivery-service/src/main/java/on/logistics/deliveryservice/global/configuration/JpaAuditingConfig.java
@@ -1,6 +1,14 @@
 package on.logistics.deliveryservice.global.configuration;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import on.logistics.deliveryservice.global.domain.Passport;
+import on.logistics.deliveryservice.global.utils.PassportUtil;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
